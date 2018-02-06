f37fd920a40c36ee2c6d9e4c6dbb4c34862760dc
danikachrunik@gmail.com Chrunik <danikachrunik@gmail.com>
2018-02-06 23:14:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function increaseRankBy(n) {
 
 function deepestChild() {
   var elem = document.getElementById('grand-node').querySelectorAll('div');
-  if(elem[0].hasChildNodes) {
+  if(elem[0].hasChildNodes()) {
     elem = elem[0].querySelectorAll('div');
   } else {
     console.log(elem);
