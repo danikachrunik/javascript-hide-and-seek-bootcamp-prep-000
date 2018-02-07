f6adff152c32ed19cec93ef77b2d0e1a7c1218d2
danikachrunik@gmail.com Chrunik <danikachrunik@gmail.com>
2018-02-07 00:29:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,12 +16,10 @@ function increaseRankBy(n) {
 function deepestChild() {
   var elem = document.getElementById('grand-node').querySelectorAll('div');
   
-  while (elem[0].hasChildNodes()) {
-    if(!elem[0].hasChildNodes()) {
-      return elem;
-    } else {
-      elem = elem[0].querySelectorAll('div');
-    }
+  if(!elem[0].hasChildNodes()) {
+    return elem;
+  } else {
+    elem = elem[0].querySelectorAll('div');
   }
 }
 
