3fc4df7df6413d81ce359f499f85cf2498eec20d
danikachrunik@gmail.com Chrunik <danikachrunik@gmail.com>
2018-02-07 01:05:09 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,18 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.getElementById('nested').getElementsByClassName('target')[0];
+}
+
+function increaseRankBy(n) {
+  var rank = document.getElementsByClassName("ranked-list");
+  for(var i = 0; i < rank.length; i++) {
+    rank[i].innerHTML = (parseInt(rank[i].innerHTML) + n);
+  }
+}
+
+function deepestChild() {
+  return document.getElementById('grand-node').querySelectorAll('div')[0].querySelectorAll('div')[0].querySelectorAll('div')[0].querySelectorAll('div')[0];
+  }
